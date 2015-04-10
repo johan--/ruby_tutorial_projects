6fcaa71e0874d4b26a2ccf9cb05f2866293243a0
Raymond Sapida <ray.sapida@gmail.com>
2015-04-09 19:28:01 -0700

    Starting point for jumpstart labs tutorial project IdeaBox
    http://tutorials.jumpstartlab.com/projects/idea_box.html

---
+++ idea_box/app.rb
@@ -0,0 +1,40 @@
+require 'bundler'
+Bundler.require
+require './idea'
+require './idea_store'
+
+class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
+  get '/' do
+    erb :index, locals: {ideas: IdeaStore.all }
+  end
+
+  post '/' do
+    IdeaStore.create(params[:idea])
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = IdeaStore.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  delete '/:id' do |id|
+    IdeaStore.delete(id.to_i)
+    redirect '/'
+  end
+
+  put '/:id' do |id|
+    IdeaStore.update(id.to_i, params[:idea])
+    redirect '/'
+  end
+end
