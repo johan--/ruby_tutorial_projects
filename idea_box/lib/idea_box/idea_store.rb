04bd568ac35f8f7f6ea251833360d6b62ff80081
Raymond Sapida <ray.sapida@gmail.com>
2015-04-09 19:59:11 -0700

    Added raning functionality to ideas
    - Added rank and id instance variables to idea class and initialization
    - Added form button in the index view for a 'like' and corresponding POST in app.rb
    - Defined #like! method as one more integer value in @rank
    - Included Comparable module in Idea class and defined the spaceship operator
    - Added a sort method to the ideas pass in the locals hash for the index
    - Changed IdeaStore.all and IdeaStore.find to merge the new id variable
    - Update views to use this new id
    - Changed IdeaStore.database to have at least an empty array when it's loaded

--- idea_box/lib/idea_box/idea_store.rb
+++ idea_box/lib/idea_box/idea_store.rb
@@ -3,15 +3,16 @@ require 'yaml/store'
 class IdeaStore
   def self.create(attributes)
     database.transaction do
-      database['ideas'] ||= []
-      database['ideas'] << attributes
+      database['ideas'] << data
     end
   end
 
   def self.all
-    raw_ideas.map do |data|
-      Idea.new(data)
+    ideas = []
+    raw_ideas.each_with_index do |data, i|
+      ideas << Idea.new(data.merge("id" => i))
     end
+    ideas
   end
 
   def self.raw_ideas
@@ -22,7 +23,13 @@ class IdeaStore
 
 
   def self.database
-    @database ||= YAML::Store.new('db/ideabox')
+    return @database if @database
+
+    @database = YAML::Store.new('db/ideabox')
+    @database.transaction do
+      @database['ideas'] ||= []
+    end
+    @database
   end
 
   def self.delete(position)
@@ -32,7 +39,8 @@ class IdeaStore
   end
 
   def self.find(id)
-    Idea.new(find_raw_idea(id))
+    raw_idea = find_raw_idea(id)
+    Idea.new(raw_idea.merge("id" => id))
   end
 
   def self.find_raw_idea(id)
