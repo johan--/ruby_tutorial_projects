6b2b170debe2365799fd4e670ea749a3a8d82afa
Raymond Sapida <ray.sapida@gmail.com>
2015-04-12 00:30:27 -0700

    Changed SECRET_NUMBER constant to @@secret_variable
    - @@secret_variable resets when the user guesses correctly or when the user uses
      up all of their guesses

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,33 +1,22 @@
 require 'sinatra'
 #require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+@@secret_number = rand(100)
 @@number_of_guesses = 5
 
 get '/' do
-  number = SECRET_NUMBER
+  number = @@secret_number
   guess = params["guess"]
   message = "Start guessing a number"
   message = check_guess(guess) unless guess.nil?
   if message.include?("limit") || message.include?("right")
-    number = SECRET_NUMBER
+    @@number_of_guesses = 5
+    @@secret_number = rand(100)
   end
   erb :index, :locals => {:number => number, :message => message}
 end
 
 def check_guess(guess)
-  # Create a class variable with @@ that keeps track of how many guesses they
-  # have remaining
-  # When subtract one from that each guess
-  # If the guesses reach zero, then…
-  # Generate a new number
-  # Set the number of guesses back to five
-  # Show them a message that they’ve lost and a new number has been generated
-  # If they guess correctly, then…
-  # Generate a new number
-  # Set the number of guesses back to five
-  # Show the message that they’ve guessed correctly
-  #
   # http://localhost:4567/?guess=56&cheat=true
   # They unlock the cheat mode. When cheat is true, the page should always print
   # out the secret number so they can get it right on the next guess.
@@ -35,17 +24,16 @@ def check_guess(guess)
   @@number_of_guesses -= 1
 
   if @@number_of_guesses == 0
-    @@number_of_guesses = 5
     return "Guess limit reached and generated a new number"
   end
 
-  if guess.to_f == SECRET_NUMBER
-    "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
-  elsif guess.to_f > (SECRET_NUMBER - 5) && guess.to_f < (SECRET_NUMBER + 5)
-    guess.to_f < SECRET_NUMBER ? "Too low!" : "Too high!"
-  elsif guess.to_f < SECRET_NUMBER
+  if guess.to_f == @@secret_number
+    "You got it right! The SECRET NUMBER is #{@@secret_number}"
+  elsif guess.to_f > (@@secret_number - 5) && guess.to_f < (@@secret_number + 5)
+    guess.to_f < @@secret_number ? "Too low!" : "Too high!"
+  elsif guess.to_f < @@secret_number
     "Way too low!"
-  elsif guess.to_f > SECRET_NUMBER
+  elsif guess.to_f > @@secret_number
     "Way too high!"
   else
     "Something went wrong."
