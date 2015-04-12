3ff3a8389e6904581d96631e107d91633d8f30f6
Raymond Sapida <ray.sapida@gmail.com>
2015-04-11 19:59:10 -0700

    First iteration of the #run_command
    - The command is split between the first letter and the rest
    - The only thing it can do currently is change the background
    - An eval function is being used and it's unsafe as is

--- process_artist/process_artist.rb
+++ process_artist/process_artist.rb
@@ -18,7 +18,20 @@ class ProcessArtist < Processing::App
       @queue = @queue + key
     else
       warn "Time to run the command: #{@queue}"
+      run_command(@queue)
       @queue = ""
+    end
+  end
+
+  def run_command(command)
+    letter = command[0]
+    args = command[1..-1].split(",").map(&:to_i)
+    puts "Running Command #{letter} : #{args}"
+    case letter
+    when "b"
+      eval("background(#{args[0]},#{args[1]},#{args[2]})")
+    else
+      puts "Something went wrong"
     end
   end
 end
