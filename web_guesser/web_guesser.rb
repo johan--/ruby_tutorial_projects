d67b4bf44b286b2522951ec4c6a5aa945ea0624c
Raymond Sapida <ray.sapida@gmail.com>
2015-04-11 22:32:08 -0700

    Added a class variable to keep track of the amount of guesses
    - Functionality is not finished because even though the class variable is reset
      after 5 counts, the SECRET_NUMBER is not regenerated.

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -2,21 +2,20 @@ require 'sinatra'
 #require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(100)
+@@number_of_guesses = 5
 
 get '/' do
   number = SECRET_NUMBER
   guess = params["guess"]
   message = "Start guessing a number"
   message = check_guess(guess) unless guess.nil?
+  if message.include?("limit") || message.include?("right")
+    number = SECRET_NUMBER
+  end
   erb :index, :locals => {:number => number, :message => message}
 end
 
 def check_guess(guess)
-  # Colorizing the Output
-  #
-  # Can you make it so they only get five guesses before a new number is
-  # generated? 
-  #
   # Create a class variable with @@ that keeps track of how many guesses they
   # have remaining
   # When subtract one from that each guess
@@ -32,7 +31,14 @@ def check_guess(guess)
   # http://localhost:4567/?guess=56&cheat=true
   # They unlock the cheat mode. When cheat is true, the page should always print
   # out the secret number so they can get it right on the next guess.
-  #
+  
+  @@number_of_guesses -= 1
+
+  if @@number_of_guesses == 0
+    @@number_of_guesses = 5
+    return "Guess limit reached and generated a new number"
+  end
+
   if guess.to_f == SECRET_NUMBER
     "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
   elsif guess.to_f > (SECRET_NUMBER - 5) && guess.to_f < (SECRET_NUMBER + 5)
