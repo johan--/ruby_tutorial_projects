cc8f03aa4b943056babf1566fb5dee24d3c6dc7b
Raymond Sapida <ray.sapida@gmail.com>
2015-04-12 14:05:33 -0700

    Test validate for presence of company name

--- contact_manager/spec/models/company_spec.rb
+++ contact_manager/spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) { Company.create(name: 'Acme') }
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'is invalid' do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
 end
