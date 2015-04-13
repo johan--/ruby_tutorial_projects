57b41589de00495dfca044c9068fe5fa4373a6b8
Raymond Sapida <ray.sapida@gmail.com>
2015-04-12 18:49:17 -0700

    Change new view headline to include company or person name
    - Change #to_s method for both company and person to show name.
    - Add model specs to test that #to_s works as expected.
    - Add views spec tests for new template in email and phone to ensure that the
      model names are being shown.

--- contact_manager/spec/models/company_spec.rb
+++ contact_manager/spec/models/company_spec.rb
@@ -21,4 +21,8 @@ RSpec.describe Company, type: :model do
     company.email_addresses.build(address: 'me@example.com')
     expect(company.email_addresses.map(&:address)).to eq(['me@example.com'])
   end
+
+  it 'converts to a string with name' do
+    expect(company.to_s).to eq('Acme')
+  end
 end
