23d14a822ee6f2c9ea09f74c87ac6da8ab152fbf
Raymond Sapida <ray.sapida@gmail.com>
2015-04-12 17:18:36 -0700

    Add tests for email address in company view spec
    - Copy email address tests from person view spec. Change person references to
      company.
    - Add new, edit, and delete links to company view.

--- contact_manager/spec/features/company_view_spec.rb
+++ contact_manager/spec/features/company_view_spec.rb
@@ -62,4 +62,67 @@ describe 'the company view', type: :feature do
     end
 
   end
+
+  describe 'for email addresses' do
+    let(:company) { Company.create(name: 'Acme') }
+
+    before(:each) do
+      company.email_addresses.create(address: 'john@example.com')
+      company.email_addresses.create(address: 'doe@example.com')
+      visit company_path(company)
+    end
+
+    it 'shows each address' do
+      expect(page).to have_selector('li', text: 'john@example.com')
+      expect(page).to have_selector('li', text: 'doe@example.com')
+    end
+
+    it 'has a link to add a new email address' do
+      expect(page).to have_link('Add email address', href: new_email_address_path(contact_id: company.id, contact_type: 'Company'))
+    end
+
+    it 'has an add email address link that can be clicked to new email address' do
+      click_link('Add email address')
+      expect(page).to have_content('New Email Address')
+    end
+
+    it 'adds a new email address' do
+      page.click_link('Add email address')
+      page.fill_in('Address', with: 'name@example.com')
+      page.click_button('Create Email address')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('name@example.com')
+    end
+
+    it 'has links to edit email address' do
+      company.email_addresses.each do |email|
+        expect(page).to have_link('edit', href: edit_email_address_path(email))
+      end
+    end
+
+    it 'edits a phone number' do
+      email = company.email_addresses.first
+      old_email = email.address
+
+      first(:link, 'edit').click
+      page.fill_in('Address', with: 'ruby@rspec.com')
+      page.click_button('Update Email address')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('ruby@rspec.com')
+      expect(page).to_not have_content(old_email)
+    end
+
+    it 'has links to delete email addresses' do
+      company.email_addresses.each do |email|
+        expect(page).to have_link('delete', href: email_address_path(email))
+      end
+    end
+
+    it 'deletes an email address' do
+      first(:link, 'delete').click
+      expect(current_path).to eq(company_path(company))
+      expect(page).to_not have_content('john@example.com')
+    end
+
+  end
 end
