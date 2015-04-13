f8862806fe2e580e7e2b563a344a6d5f15d49ac6
Raymond Sapida <ray.sapida@gmail.com>
2015-04-13 16:14:43 -0700

    Create has_many association between people and user
    - Add migration to add user_id to people table.
    - Add Fabricator gem and configure it to be used.
    - Create a user fabricator.

--- contact_manager/db/schema.rb
+++ contact_manager/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413021028) do
+ActiveRecord::Schema.define(version: 20150413231024) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150413021028) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "phone_numbers", force: :cascade do |t|
--- contact_manager/spec/models/user_spec.rb
+++ contact_manager/spec/models/user_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { Fabricate(:user) }
+
   describe 'find_or_create_by_auth' do
     it { expect(User).to respond_to(:find_or_create_by_auth).with(1).argument }
+  end
+
+  it 'has associated people' do
+    expect(user).to respond_to(:people)
   end
 end
