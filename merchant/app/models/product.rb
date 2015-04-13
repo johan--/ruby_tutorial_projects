9e560c7d4964bf5e762336d77b7c53959df34d18
Raymond Sapida <ray.sapida@gmail.com>
2015-04-13 05:43:34 -0700

    Add validation for numericality of price and update model spec

--- merchant/app/models/product.rb
+++ merchant/app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   validates :title, :price, :description, presence: true
+  validates_numericality_of :price
 end
--- merchant/spec/models/product_spec.rb
+++ merchant/spec/models/product_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:description) }
+  it { should validate_numericality_of(:price) }
 end
