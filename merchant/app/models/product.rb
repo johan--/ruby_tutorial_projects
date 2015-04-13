fffffba6f3fbf0efe23a3e1e9c816b105fe403d3
Raymond Sapida <ray.sapida@gmail.com>
2015-04-13 09:04:19 -0700

    Add stock to price and default it to 0
    - #print_stock helper method to explicitly say out of stock if stock is 0.
    - Add validation for numericality of stock and update model spec.

--- merchant/app/models/product.rb
+++ merchant/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   validates :title, :price, :description, presence: true
   validates_numericality_of :price
+  validates_numericality_of :stock
 end
--- merchant/spec/models/product_spec.rb
+++ merchant/spec/models/product_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:description) }
   it { should validate_numericality_of(:price) }
+  it { should validate_numericality_of(:stock) }
 end
