e84e45199e1136ba75143bcab133e1b3f544dfb9
Raymond Sapida <ray.sapida@gmail.com>
2015-04-13 17:02:46 -0700

    Continue writing integration test and scope to current user.
    - Fake a login and include in spec/support. Add configuration in rails helper.
    - Update tests to reflect change and delete spec/request/people_spec because it
      copies another test.

--- contact_manager/spec/controllers/people_controller_spec.rb
+++ contact_manager/spec/controllers/people_controller_spec.rb
@@ -15,11 +15,12 @@ RSpec.describe PeopleController, type: :controller do
   # PeopleController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "assigns all people as @people" do
-      person = Person.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:people)).to eq([person])
+  describe "GET index" do
+    it "assigns the current user's people" do
+      user = User.create
+      person = Person.create! valid_attributes.merge(user_id: user.id)
+      get :index, {}, {:user_id => user.id}
+      assigns(:people).should eq([person])
     end
   end
 
--- contact_manager/spec/fabricators/people_view_spec.rb
+++ contact_manager/spec/fabricators/people_view_spec.rb
@@ -6,6 +6,9 @@ describe 'the people view', type: :feature do
 
   context 'when logged in' do
     let(:user) { Fabricate(:user) }
+    before do
+      login_as(user)
+    end
 
     it 'displays people associated with the user' do
       person_1 = Fabricate(:person)
@@ -13,6 +16,15 @@ describe 'the people view', type: :feature do
       person_1.save
       visit(people_path)
       expect(page).to have_text(person_1.to_s)
+    end
+
+    it "does not display people associated with another user" do
+      user_2 = Fabricate(:user)
+      person_2 = Fabricate(:person)
+      person_2.user = user_2
+      person_2.save
+      visit(people_path)
+      expect(page).not_to have_text(person_2.to_s)
     end
 
   end
