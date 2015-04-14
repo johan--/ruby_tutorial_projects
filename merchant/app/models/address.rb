c958bd7c417c0433639925ac7a0c2f9b6a453ad8
Raymond Sapida <ray.sapida@gmail.com>
2015-04-14 14:23:59 -0700

    Displaying address in orders#show and changing #to_s in address model

--- merchant/app/models/address.rb
+++ merchant/app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ActiveRecord::Base
   belongs_to :user
   has_many :orders
   validates :line1, :city, :state, :zip, :user_id, presence: true
+
+  def to_s
+    [line1, line2, city, state, zip].compact.join(", ")
+  end
 end
