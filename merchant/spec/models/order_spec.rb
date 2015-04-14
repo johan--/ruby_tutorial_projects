a323bfceb332351c5d7c2ea5756c7c006e806073
Raymond Sapida <ray.sapida@gmail.com>
2015-04-14 15:52:40 -0700

    Add tests for order associations to user, address, and order items

--- merchant/spec/models/order_spec.rb
+++ merchant/spec/models/order_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it { should have_many(:order_items) }
+  it { should belong_to(:user) }
+  it { should belong_to(:address) }
+  it { should have_many(:order_items).dependent(:destroy) }
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:status) }
 end
