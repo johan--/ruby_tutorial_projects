1a6fdf3a06f8e9a7c8156e8086cb52aa0075b78f
Raymond Sapida <ray.sapida@gmail.com>
2015-04-14 13:25:09 -0700

    Create resources for sessions and add links for logging in and out

--- merchant/config/routes.rb
+++ merchant/config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :order_items, except: [:show, :index, :new]
   resources :products
 
+  resource :sessions, :only => [:create, :destroy]
   get '/auth/:provider/callback' => 'sessions#create'
+
+  get "/login" => redirect("/auth/twitter"), as: :login
+  delete "/logout" => "sessions#destroy", as: :logout
 end
