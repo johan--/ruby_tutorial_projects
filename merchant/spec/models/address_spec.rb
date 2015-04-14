b0f12e69da7fa414359f6353246d0b445ba81246
Raymond Sapida <ray.sapida@gmail.com>
2015-04-14 15:42:28 -0700

    Add tests for the presence validations in address model

--- merchant/spec/models/address_spec.rb
+++ merchant/spec/models/address_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:line1) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_presence_of(:zip) }
+  it { should validate_presence_of(:zip) }
 end
