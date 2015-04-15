ba11b957e35de14eff673727d31af19968d6f294
Raymond Sapida <ray.sapida@gmail.com>
2015-04-15 09:59:37 -0700

    Add presence validation for content in micropost

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user_id
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }, presence: true
 end
--- toy_app/spec/models/micropost_spec.rb
+++ toy_app/spec/models/micropost_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Micropost, type: :model do
   it { should belong_to(:user) }
   it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:content) }
   it { should validate_length_of(:content).is_at_most(140) }
 end
