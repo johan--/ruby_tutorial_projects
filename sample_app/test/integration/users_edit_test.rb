1e67e033d4d0a60243b0eaa232361a8d2921cf9d
Raymond Sapida <ray.sapida@gmail.com>
2015-04-15 15:56:12 -0700

    Misplaced controller test in integration test

--- sample_app/test/integration/users_edit_test.rb
+++ sample_app/test/integration/users_edit_test.rb
@@ -34,18 +34,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal @user.email, email
   end
 
-  test "should redirect edit when not logged in" do
-    get :edit, id: @user
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "should redirect update when not logged in" do
-    patch :update, id: @user, user: { name: @user.name, email: @user.email }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
     log_in_as(@user)
