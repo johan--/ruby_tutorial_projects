9b4286240a19cc719546efc2ed73c3c13a2e8771
Raymond Sapida <ray.sapida@gmail.com>
2015-04-15 09:50:28 -0700

    Add association between micropost and user
    - Add validations for micropost fields.
    - Add tests for validations and associations.

--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   validates_presence_of :name, :email
 end
--- toy_app/spec/models/micropost_spec.rb
+++ toy_app/spec/models/micropost_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_length_of(:content).is_at_most(140) }
 end
