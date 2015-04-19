d6ae54f8b84c25642ccb1c2cc32ace38172e1d49
Raymond Sapida <ray.sapida@gmail.com>
2015-04-18 19:43:39 -0700

    Include sorcery test helpers
    https://github.com/NoamB/sorcery/wiki/Testing-Rails
    - Makes login_user and logout_user available to controller and integration test.
      Still need to set up a user fixture or factory.

--- blogger/spec/rails_helper.rb
+++ blogger/spec/rails_helper.rb
@@ -26,6 +26,8 @@ require 'paperclip/matchers'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
+  config.include Sorcery::TestHelpers::Rails::Integration, type: :feature
   config.include Paperclip::Shoulda::Matchers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
