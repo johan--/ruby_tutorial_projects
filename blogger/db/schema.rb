de798cb173f95d40b16000bfe1e1ca901ee98eee
Raymond Sapida <ray.sapida@gmail.com>
2015-04-18 20:00:48 -0700

    Generate migration to add slugs to pages

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419014849) do
+ActiveRecord::Schema.define(version: 20150419025918) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150419014849) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
   create_table "taggings", force: :cascade do |t|
