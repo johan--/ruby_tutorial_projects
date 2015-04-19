21a71d979490219e4e0d16470256ddf62da96639
Raymond Sapida <ray.sapida@gmail.com>
2015-04-18 19:52:30 -0700

    Comment out articles search for tag name in index

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -2,7 +2,8 @@ class ArticlesController < ApplicationController
   before_filter :require_login, except: [:index, :show]
 
   def index
-    @articles, @tag = Article.search_by_tag_name(params[:tag])
+    #@articles, @tag = Article.search_by_tag_name(params[:tag])
+    @articles = Article.all
     @pages = Page.all
   end
   
