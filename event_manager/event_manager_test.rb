883d8a53d373ad543fc8c6173e41ea40d7bb99a8
Raymond Sapida <ray.sapida@gmail.com>
2015-04-20 08:01:07 -0700

    Test that clean_phone parses hyphens from string

--- event_manager/event_manager_test.rb
+++ event_manager/event_manager_test.rb
@@ -4,6 +4,11 @@ require_relative 'lib/event_manager'
 class EventManagerTest < MiniTest::Unit::TestCase
   def test_that_clean_phone_method_returns_same_number_when_formatted_correcty
     parsing = ParsingData.new
-    assert_equal parsing.clean_phone(6154385000), "6154385000"
+    assert_equal parsing.clean_phone("6154385000"), "6154385000"
+  end
+
+  def test_that_clean_phone_method_parsing_hyphens_from_string
+    parsing = ParsingData.new
+    assert_equal parsing.clean_phone("414-520-5000"), "4145205000"
   end
 end
