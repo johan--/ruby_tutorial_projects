37da6e0d8285c808478acaff82571b047b04459c
Raymond Sapida <ray.sapida@gmail.com>
2015-04-20 09:08:43 -0700

    Edit phone_number_spec to use shoulda matchers syntax

--- contact_manager/app/models/phone_number.rb
+++ contact_manager/app/models/phone_number.rb
@@ -1,4 +1,4 @@
 class PhoneNumber < ActiveRecord::Base
   belongs_to :contact, polymorphic: true
-  validates :number, :contact_id, presence: true
+  validates_presence_of :number, :contact_id
 end
--- contact_manager/spec/models/phone_number_spec.rb
+++ contact_manager/spec/models/phone_number_spec.rb
@@ -4,21 +4,11 @@ RSpec.describe PhoneNumber, type: :model do
   let(:person) { Person.create(:first_name => "Jimbob", :last_name => "Billy") }
   let(:phone_number) { PhoneNumber.new(number: "1112223333", contact_id: person.id, contact_type: 'Person') }
 
+  it { should belong_to(:contact) }
+  it { should validate_presence_of(:number) }
+  it { should validate_presence_of(:contact_id) }
+
   it 'is valid' do
     expect(phone_number).to be_valid
-  end
-
-  it 'is invalid without a number' do
-    phone_number.number = nil
-    expect(phone_number).to_not be_valid
-  end
-
-  it 'must have a reference to a person' do
-    phone_number.contact_id = nil
-    expect(phone_number).to_not be_valid
-  end
-
-  it 'is associated with a contact' do
-    expect(phone_number).to respond_to(:contact)
   end
 end
