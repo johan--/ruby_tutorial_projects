4714038057ec39988e7b6e6a5b4bd24067ffcf9b
Raymond Sapida <ray.sapida@gmail.com>
2015-04-19 20:48:39 -0700

    Add test for supported characters

--- encryptor/encryptor_test.rb
+++ encryptor/encryptor_test.rb
@@ -38,4 +38,19 @@ class EncryptorTest < MiniTest::Unit::TestCase
 
     assert_equal encryptor.decrypt("Uryy!", 13), "Hello"
   end
+
+  def test_supported_characters
+    characters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+",
+                  ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7",
+                  "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C",
+                  "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+                  "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[",
+                  "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g",
+                  "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                  "t", "u", "v", "w", "x", "y", "z"]
+
+    encryptor = Encryptor.new
+
+    assert_equal encryptor.supported_characters, characters
+  end
 end
