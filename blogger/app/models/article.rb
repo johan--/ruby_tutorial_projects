67f84440eda064872b362c60fa69ec5b1a61f8b3
Raymond Sapida <ray.sapida@gmail.com>
2015-04-19 18:44:40 -0700

    Add presence validations for title and body in article model

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
+  validates_presence_of :title, :body
+
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
