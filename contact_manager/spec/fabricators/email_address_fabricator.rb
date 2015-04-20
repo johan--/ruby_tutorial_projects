2add31ac901ace0dd82c563ee330ad3bf90f2ac7
Raymond Sapida <ray.sapida@gmail.com>
2015-04-20 09:45:34 -0700

    Use faker email for email address fabricator and create second fabricot for company contacts

--- contact_manager/spec/fabricators/email_address_fabricator.rb
+++ contact_manager/spec/fabricators/email_address_fabricator.rb
@@ -1,5 +1,10 @@
 Fabricator(:email_address) do
-  address "email@example.com"
+  address { Faker::Internet.email }
   contact_id { Fabricate(:person).id }
   contact_type "Person"
+end
+
+Fabricator(:email_address_from_company, from: :email_address) do
+  contact_id { Fabricate(:company).id }
+  contact_type "Company"
 end
--- contact_manager/spec/models/email_address_spec.rb
+++ contact_manager/spec/models/email_address_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe EmailAddress, type: :model do
   let(:person) { Fabricate(:person) }
   let(:email_address) { Fabricate(:email_address) }
+  let(:email_address_from_company) { Fabricate(:email_address_from_company) }
 
   it { should belong_to(:contact) }
   it { should validate_presence_of(:address) }
@@ -10,5 +11,9 @@ RSpec.describe EmailAddress, type: :model do
 
   it 'is valid' do
     expect(email_address).to be_valid
+  end
+
+  it 'email address from company is valid' do
+    expect(email_address_from_company).to be_valid
   end
 end
