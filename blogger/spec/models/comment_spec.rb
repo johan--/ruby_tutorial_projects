90debf1d2d1e1e09f019af8fca67fa5492e7c94f
Raymond Sapida <ray.sapida@gmail.com>
2015-04-19 19:44:00 -0700

    Create comment fabricator and check that it's valid

--- blogger/spec/models/comment_spec.rb
+++ blogger/spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:comment) { Fabricate(:comment) }
+
   it { should belong_to(:article) }
+
+  it 'is valid' do
+    expect(comment).to be_valid
+  end
 end
