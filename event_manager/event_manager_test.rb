82f374a340605669ab154e28db559cbc70f66127
Raymond Sapida <ray.sapida@gmail.com>
2015-04-20 07:54:43 -0700

    Test that clean_phone returns self when number is formatted correctly

--- event_manager/event_manager_test.rb
+++ event_manager/event_manager_test.rb
@@ -2,4 +2,8 @@ require 'minitest/autorun'
 require_relative 'lib/event_manager'
 
 class EventManagerTest < MiniTest::Unit::TestCase
+  def test_that_clean_phone_method_returns_same_number_when_formatted_correcty
+    parsing = ParsingData.new
+    assert_equal parsing.clean_phone(6154385000), "6154385000"
+  end
 end
