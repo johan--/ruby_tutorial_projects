297fdd09ee86071f1173555c2b34c36c64441e7f
Raymond Sapida <ray.sapida@gmail.com>
2015-04-21 18:54:29 -0700

    Add valid, invalid and new attributes to topics controller spec

--- suggestotron/spec/controllers/topics_controller_spec.rb
+++ suggestotron/spec/controllers/topics_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe TopicsController, type: :controller do
   # Topic. As you add validations to Topic, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { title: "Title", description: "Description" }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { title: nil, description: nil }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -103,14 +103,15 @@ RSpec.describe TopicsController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { title: "New Title", description: "New Description" }
       }
 
       it "updates the requested topic" do
         topic = Topic.create! valid_attributes
         put :update, {:id => topic.to_param, :topic => new_attributes}, valid_session
         topic.reload
-        skip("Add assertions for updated state")
+        expect(topic.title).to eq("New Title")
+        expect(topic.description).to eq("New Description")
       end
 
       it "assigns the requested topic as @topic" do
