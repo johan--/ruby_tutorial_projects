1d24bb00231462637eb8a277c9859c748153df6f
Raymond Sapida <ray.sapida@gmail.com>
2015-04-21 19:08:55 -0700

    Add upvote action to topics controller

--- suggestotron/app/controllers/topics_controller.rb
+++ suggestotron/app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+  before_action :set_topic, only: [:show, :edit, :update, :destroy, :upvote]
 
   # GET /topics
   # GET /topics.json
@@ -61,14 +61,19 @@ class TopicsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_topic
-      @topic = Topic.find(params[:id])
-    end
+  def upvote
+    @topic.votes.create
+    redirect_to(topics_path)
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def topic_params
-      params.require(:topic).permit(:title, :description)
-    end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def topic_params
+    params.require(:topic).permit(:title, :description)
+  end
 end
