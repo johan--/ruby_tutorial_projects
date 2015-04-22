139c35322aa7242f7de9460f882b5790370cdabe
Raymond Sapida <ray.sapida@gmail.com>
2015-04-22 16:18:59 -0700

    Update replies controller spec

--- message_board/spec/controllers/replies_controller_spec.rb
+++ message_board/spec/controllers/replies_controller_spec.rb
@@ -19,16 +19,20 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe RepliesController, type: :controller do
+  let(:user) { User.create(email: "example@rails.com",
+                           password: "password",
+                           password_confirmation: "password") }
+  let(:post) { Post.create(title: "Title", content: "content", user_id: user.id) }
 
   # This should return the minimal set of attributes required to create a valid
   # Reply. As you add validations to Reply, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { content: "Reply", post_id: post.id, user_id: user.id }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { content: nil, post_id: nil, user_id: nil}
   }
 
   # This should return the minimal set of values that should be in the session
@@ -38,31 +42,35 @@ RSpec.describe RepliesController, type: :controller do
 
   describe "GET #index" do
     it "assigns all replies as @replies" do
-      reply = Reply.create! valid_attributes
-      get :index, {}, valid_session
+      sign_in user
+      reply = post.replies.create! valid_attributes
+      get :index, {:post_id => post.id}, valid_session
       expect(assigns(:replies)).to eq([reply])
     end
   end
 
   describe "GET #show" do
     it "assigns the requested reply as @reply" do
-      reply = Reply.create! valid_attributes
-      get :show, {:id => reply.to_param}, valid_session
+      sign_in user
+      reply = post.replies.create! valid_attributes
+      get :show, {:id => reply.to_param, :post_id => post.id}, valid_session
       expect(assigns(:reply)).to eq(reply)
     end
   end
 
   describe "GET #new" do
     it "assigns a new reply as @reply" do
-      get :new, {}, valid_session
+      sign_in user
+      get :new, {:post_id => post.id}, valid_session
       expect(assigns(:reply)).to be_a_new(Reply)
     end
   end
 
   describe "GET #edit" do
     it "assigns the requested reply as @reply" do
-      reply = Reply.create! valid_attributes
-      get :edit, {:id => reply.to_param}, valid_session
+      sign_in user
+      reply = post.replies.create! valid_attributes
+      get :edit, {:id => reply.to_param, :post_id => post.id}, valid_session
       expect(assigns(:reply)).to eq(reply)
     end
   end
@@ -70,31 +78,36 @@ RSpec.describe RepliesController, type: :controller do
   describe "POST #create" do
     context "with valid params" do
       it "creates a new Reply" do
+        sign_in user
         expect {
-          post :create, {:reply => valid_attributes}, valid_session
+          post :create, {:post_id => post.id, :reply => valid_attributes}, valid_session
         }.to change(Reply, :count).by(1)
       end
 
       it "assigns a newly created reply as @reply" do
-        post :create, {:reply => valid_attributes}, valid_session
+        sign_in user
+        post :create, {:post_id => post.id, :reply => valid_attributes}, valid_session
         expect(assigns(:reply)).to be_a(Reply)
         expect(assigns(:reply)).to be_persisted
       end
 
       it "redirects to the created reply" do
-        post :create, {:reply => valid_attributes}, valid_session
+        sign_in user
+        post :create, {:post_id => post.id, :reply => valid_attributes}, valid_session
         expect(response).to redirect_to(Reply.last)
       end
     end
 
     context "with invalid params" do
       it "assigns a newly created but unsaved reply as @reply" do
-        post :create, {:reply => invalid_attributes}, valid_session
+        sign_in user
+        post :create, {:post_id => post.id, :reply => invalid_attributes}, valid_session
         expect(assigns(:reply)).to be_a_new(Reply)
       end
 
       it "re-renders the 'new' template" do
-        post :create, {:reply => invalid_attributes}, valid_session
+        sign_in user
+        post :create, {:post_id => post.id, :reply => invalid_attributes}, valid_session
         expect(response).to render_template("new")
       end
     end
@@ -103,39 +116,44 @@ RSpec.describe RepliesController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { content: "New content" }
       }
 
       it "updates the requested reply" do
-        reply = Reply.create! valid_attributes
-        put :update, {:id => reply.to_param, :reply => new_attributes}, valid_session
+        sign_in user
+        reply = post.replies.create! valid_attributes
+        put :update, {:id => reply.to_param, :reply => new_attributes, :post_id => post.id}, valid_session
         reply.reload
-        skip("Add assertions for updated state")
+        expect(reply.content).to eq("New content")
       end
 
       it "assigns the requested reply as @reply" do
-        reply = Reply.create! valid_attributes
-        put :update, {:id => reply.to_param, :reply => valid_attributes}, valid_session
+        sign_in user
+        reply = post.replies.create! valid_attributes
+        put :update, {:id => reply.to_param, :reply => valid_attributes, :post_id => post.id}, valid_session
         expect(assigns(:reply)).to eq(reply)
       end
 
       it "redirects to the reply" do
-        reply = Reply.create! valid_attributes
-        put :update, {:id => reply.to_param, :reply => valid_attributes}, valid_session
-        expect(response).to redirect_to(reply)
+        sign_in user
+        reply = post.replies.create! valid_attributes
+        put :update, {:id => reply.to_param, :reply => valid_attributes, :post_id => post.id}, valid_session
+        expect(response).to redirect_to(post)
       end
     end
 
     context "with invalid params" do
       it "assigns the reply as @reply" do
-        reply = Reply.create! valid_attributes
-        put :update, {:id => reply.to_param, :reply => invalid_attributes}, valid_session
+        sign_in user
+        reply = post.replies.create! valid_attributes
+        put :update, {:id => reply.to_param, :reply => invalid_attributes, :post_id => post.id}, valid_session
         expect(assigns(:reply)).to eq(reply)
       end
 
       it "re-renders the 'edit' template" do
-        reply = Reply.create! valid_attributes
-        put :update, {:id => reply.to_param, :reply => invalid_attributes}, valid_session
+        sign_in user
+        reply = post.replies.create! valid_attributes
+        put :update, {:id => reply.to_param, :reply => invalid_attributes, :post_id => post.id}, valid_session
         expect(response).to render_template("edit")
       end
     end
@@ -143,16 +161,18 @@ RSpec.describe RepliesController, type: :controller do
 
   describe "DELETE #destroy" do
     it "destroys the requested reply" do
-      reply = Reply.create! valid_attributes
+      sign_in user
+      reply = post.replies.create! valid_attributes
       expect {
-        delete :destroy, {:id => reply.to_param}, valid_session
+        delete :destroy, {:id => reply.to_param, :post_id => post.id}, valid_session
       }.to change(Reply, :count).by(-1)
     end
 
     it "redirects to the replies list" do
-      reply = Reply.create! valid_attributes
-      delete :destroy, {:id => reply.to_param}, valid_session
-      expect(response).to redirect_to(replies_url)
+      sign_in user
+      reply = post.replies.create! valid_attributes
+      delete :destroy, {:id => reply.to_param, :post_id => post.id}, valid_session
+      expect(response).to redirect_to(post)
     end
   end
 
