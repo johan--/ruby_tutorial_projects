f13e852df9d050dc3fe02ada7a19ab50183d1e15
Raymond Sapida <ray.sapida@gmail.com>
2015-04-22 12:20:18 -0700

    Generate scaffold for post
    > rails g scaffold post title:text content:text user:belongs_to

--- message_board/db/schema.rb
+++ message_board/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422181341) do
+ActiveRecord::Schema.define(version: 20150422191905) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                          null: false
