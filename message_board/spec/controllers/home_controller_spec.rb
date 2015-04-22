16c52fbec35ad446d687236b0549ef552e1bad0a
Raymond Sapida <ray.sapida@gmail.com>
2015-04-22 12:16:52 -0700

    Add devise test helpers and sign in user for home controller spec

--- message_board/spec/controllers/home_controller_spec.rb
+++ message_board/spec/controllers/home_controller_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET index" do 
+    let(:user) { User.create(email: "example@rails.com",
+                             password: "password",
+                             password_confirmation: "password") }
+   
     it "render index view" do
+      sign_in user
       get :index
       expect(response).to render_template("index")
     end
