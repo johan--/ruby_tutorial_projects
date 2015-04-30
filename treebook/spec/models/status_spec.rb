1ec652937ccc5367e83bc8ce33e23695c1b72c7a
Raymond Sapida <ray.sapida@gmail.com>
2015-04-30 13:34:55 -0700

    Add validations for status and associated model test

--- treebook/spec/models/status_spec.rb
+++ treebook/spec/models/status_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Status, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+
+  it { should validate_presence_of(:content)}
+  it { should validate_presence_of(:user_id)}
+  it { should validate_length_of(:content).is_at_least(2) }
 end
