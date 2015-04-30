7dfbe8df1ba9725e96eb2cdbf0d74f61787e1359
Raymond Sapida <ray.sapida@gmail.com>
2015-04-30 13:28:58 -0700

    Add validations for user and test in spec and create an associated factory

--- treebook/app/models/user.rb
+++ treebook/app/models/user.rb
@@ -2,5 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :statuses
+  validates_presence_of :first_name
+
+  validates_presence_of :last_name
+
+  validates :profile_name,
+            presence: true,
+            uniqueness: true,
+            format: {
+              with: /\A[a-zA-Z0-9_\-]+\z/,
+              message: 'Must be formatted correctly.'
+            }
 end
--- treebook/spec/models/user_spec.rb
+++ treebook/spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  it { should have_many(:statuses) }
+
+  it { should validate_presence_of(:first_name)}
+  it { should validate_presence_of(:last_name)}
+  it { should validate_presence_of(:profile_name)}
+  it { should validate_uniqueness_of(:profile_name)}
+
+  it { should_not allow_value('ex@mple').for(:profile_name) }
+  it { should_not allow_value('ex.ample').for(:profile_name) }
+  it { should_not allow_value('exam ple').for(:profile_name) }
+
+  it 'has a valid factory' do
+    expect(user).to be_valid
+  end
 end
