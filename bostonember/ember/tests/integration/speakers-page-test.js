6879ead1f07cb0fc02c7d25fc6bfbc30222c4571
Raymond Sapida <ray.sapida@gmail.com>
2015-05-12 04:27:27 -0700

    Add a presentations model and edit speakers model

--- bostonember/ember/tests/integration/speakers-page-test.js
+++ bostonember/ember/tests/integration/speakers-page-test.js
@@ -9,23 +9,23 @@ module('Integration - Speaker Page', {
   beforeEach: function() {
     App = startApp();
     var speakers = [
-      {
-        id: 1,
-        name: 'Bugs Bunny'
-      },
-      {
-        id: 2,
-        name: 'Wile E. Coyote'
-      },
-      {
-        id: 3,
-        name: 'Yosemite Sam'
-      }
+      { id: 1, name: 'Bugs Bunny', presentation_ids: [1,2] },
+      { id: 2, name: 'Wile E. Coyote', presentation_ids: [3] },
+      { id: 3, name: 'Yosemite Sam', presentation_ids: [4,5,6] }
+    ];
+
+    var presentations = [
+      { id: 1, title: "What's up with Docs?", speaker_id: 1 },
+      { id: 2, title: "Of course, you know, this means war.", speaker_id: 1 },
+      { id: 3, title: "Getting the most from the Acme categlog.", speaker_id: 2 },
+      { id: 4, title: "Shaaaad up!", speaker_id: 3 },
+      { id: 5, title: "Ah hates rabbits.", speaker_id: 3 },
+      { id: 6, title: "The Great horni-todes", speaker_id: 3 }
     ];
 
     server = new Pretender(function() {
       this.get('/api/speakers', function(request) {
-        return [200, {"Content-Type": "application/json"}, JSON.stringify({speakers: speakers})];
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({speakers: speakers, presentations: presentations})];
       });
 
       this.get('/api/speakers/:id', function(request) {
@@ -35,7 +35,7 @@ module('Integration - Speaker Page', {
           }
         });
 
-        return [200, {"Content-Type": "application/json"}, JSON.stringify({speaker: speaker})];
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({speaker: speaker, presentations: presentations})];
       });
     });
 
@@ -54,11 +54,11 @@ test('Should allow navigation to the speakers page from the landing page', funct
   });
 });
 
-test('Should list all speakers', function(assert) {
-  visit('/speakers').then(function() {
-    assert.equal(find('a:contains("Bugs Bunny")').length, 1);
-    assert.equal(find('a:contains("Wile E. Coyote")').length, 1);
-    assert.equal(find('a:contains("Yosemite Sam")').length, 1);
+test('Should list all speakers and number of presentations', function() {
+  visit('/speakers').then(function(assert) {
+    assert.equal(find('a:contains("Bugs Bunny (2)")').length, 1);
+    assert.equal(find('a:contains("Wile E. Coyote (1)")').length, 1);
+    assert.equal(find('a:contains("Yosemite Sam (3)")').length, 1);
   });
 });
 
