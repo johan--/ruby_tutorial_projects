b4d857cdc0fa5ab819193e29351e745e3e24a1d3
Raymond Sapida <ray.sapida@gmail.com>
2015-05-12 20:02:47 -0700

    Add a dashboard#show action for logged in users
    - Tutorial constrained the dashboard to subdomain and originally had the code
      `request.subdomain.present? &amp;&amp; request.subdomain != "www"`
    - Ruby could not recognize `&amp;&amp;` and replace it with `&&`. Reference:
    
    http://railscasts.com/episodes/221-subdomains-in-rails-3

--- getwolfpack/config/routes.rb
+++ getwolfpack/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  class SubdomainRequired
+    def self.matches?(request)
+      request.subdomain.present? && request.subdomain != "www"
+    end
+  end
+
   devise_for :users
   root 'welcome#index'
 
   resource :account, only: [:new, :create]
+  constraints SubdomainRequired do
+    root to: 'dashboards#show', as: 'subdomain_root'
+  end
 end
