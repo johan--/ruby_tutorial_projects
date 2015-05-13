f91a1e58dc8365e2d24b89b1520ce96cc91c7086
Raymond Sapida <ray.sapida@gmail.com>
2015-05-12 18:59:20 -0700

    Implement create action for accounts controller

--- getwolfpack/spec/controllers/accounts_controller_spec.rb
+++ getwolfpack/spec/controllers/accounts_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AccountsController, type: :controller do
+  let(:account_attributes) do
+    FactoryGirl.attributes_for(:account).
+      merge(owner_attributes: FactoryGirl.attributes_for(:user))
+  end
+
   describe '#new' do
     it 'shows new account form' do
       get :new
@@ -9,6 +14,24 @@ RSpec.describe AccountsController, type: :controller do
       expect(response).to render_template(:new)
       expect(assigns(:account)).to be_instance_of Account
       expect(assigns(:account).owner).to be_instance_of User
+    end
+  end
+
+  describe '#create' do
+    it 'creates new account' do
+      expect {
+        expect {
+          post :create, account: account_attributes
+        }.to change(User, :count).by(1)
+      }.to change(Account, :count).by(1)
+
+      expect(response).to redirect_to(root_url)
+    end
+
+    it 'renders form for invalid params' do
+      post :create, account: { owner_attributes: { first_name: '' } }
+
+      expect(response).to render_template(:new)
     end
   end
 
