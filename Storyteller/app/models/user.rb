113ab4cc1af38bc5d01df32e3d73575f374224e7
Raymond Sapida <ray.sapida@gmail.com>
2015-05-15 13:28:02 -0700

    Add omniauth initializer and add it to User model
    - Add current_user helper method and sessions controller.

--- Storyteller/app/models/user.rb
+++ Storyteller/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :stories
+
+  def self.from_omniauth(auth)
+    user = User.find_or_initialize_by(uid: auth['uid'])
+    user.name = auth['info']['name']
+    user.avatar_url = auth['info']['image']
+    user.save!
+    user
+  end
 end
