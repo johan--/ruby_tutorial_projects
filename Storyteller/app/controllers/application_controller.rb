b46d21a3f3c1d656eed67e77720ab003102705ee
Raymond Sapida <ray.sapida@gmail.com>
2015-05-15 14:23:14 -0700

    Add current_user to owner of the activity
    - Update public_activity partials to include owner

--- Storyteller/app/controllers/application_controller.rb
+++ Storyteller/app/controllers/application_controller.rb
@@ -3,12 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
+  include PublicActivity::StoreController
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
+
+  hide_action :current_user
 
 end
--- Storyteller/app/models/story.rb
+++ Storyteller/app/models/story.rb
@@ -6,4 +6,5 @@ class Story < ActiveRecord::Base
 
   include PublicActivity::Model
   tracked
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
 end
