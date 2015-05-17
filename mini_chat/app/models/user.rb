3e95f71a36e7fd3c1fbd0c2459df36dd79db8156
Raymond Sapida <ray.sapida@gmail.com>
2015-05-17 10:26:04 -0700

    Implement omniauth with facebook and twitter
    - Install omniauth gems for facebook and twitter and add their initializer file.
    - Generate user model with omniauth needed fields.
    - Create routes and controller actions for signing in and out.

---
+++ mini_chat/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    provider = auth.provider
+    uid = auth.uid
+    info = auth.info.symbolize_keys!
+    user = User.find_or_initialize_by(uid: uid, provider: provider)
+    user.name = info.name
+    user.avatar_url = info.image
+    user.profile_url = info.urls.send(provider.capitalize.to_sym)
+    user.save!
+    user
+  end
+end
---
+++ mini_chat/spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+avatar_url "MyString"
+provider "MyString"
+profile_url "MyString"
+uid "MyString"
+  end
+
+end
