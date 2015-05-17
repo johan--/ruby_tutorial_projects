e69bf0a09059edea2b7a9b75eddd5ba92623dde9
Raymond Sapida <ray.sapida@gmail.com>
2015-05-17 10:42:30 -0700

    Add bootstrap stylings to layout

--- mini_chat/config/routes.rb
+++ mini_chat/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'comments#new'
   resources :comments, only: [:new, :create]
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#auth_fail'
