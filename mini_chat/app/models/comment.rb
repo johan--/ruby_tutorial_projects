442b18fd341d472b6ccb5ad3e8908353e7619bce
Raymond Sapida <ray.sapida@gmail.com>
2015-05-17 10:53:03 -0700

    Add a rake task to remove excessive comments
    - Uses Heroku Scheduler add on
    > heroku addons:add scheduler:standard
    > heroku run rake remove_excessive_comments
    > heroku addons:open scheduler

--- mini_chat/app/models/comment.rb
+++ mini_chat/app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   validates :body, presence: true, length: {maximum: 2000}
+
+  def self.remove_excessive!
+    if all.count > 100
+      order('created_at ASC').limit(all.count - 50).destroy_all
+    end
+  end
 end
