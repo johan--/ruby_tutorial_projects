671c68a1e4eb4f9bf7a2f1ebadeacd09c41aab0d
Raymond Sapida <ray.sapida@gmail.com>
2015-05-17 01:16:51 -0700

    Add two migrations to use nesting comments
    - Add parent id to comments table.
    - Create a comment hierarchies table.

--- nested_comments/db/schema.rb
+++ nested_comments/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517054246) do
+ActiveRecord::Schema.define(version: 20150517075852) do
+
+  create_table "comment_hierarchies", id: false, force: :cascade do |t|
+    t.integer "ancestor_id",   null: false
+    t.integer "descendant_id", null: false
+    t.integer "generations",   null: false
+  end
+
+  add_index "comment_hierarchies", ["ancestor_id", "descendant_id", "generations"], name: "comment_anc_desc_udx", unique: true
+  add_index "comment_hierarchies", ["descendant_id"], name: "comment_desc_idx"
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150517054246) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "parent_id"
   end
 
 end
