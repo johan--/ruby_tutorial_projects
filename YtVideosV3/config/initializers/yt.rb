eb195e54a293e6848b8a5f32b06a61e78fdca8ae
Raymond Sapida <ray.sapida@gmail.com>
2015-05-19 05:38:40 -0700

    Add a yt initializer

---
+++ YtVideosV3/config/initializers/yt.rb
@@ -0,0 +1,7 @@
+Yt.configure do |config|
+  # Enable these options in APIs page at Google developer console
+  # Google+ API
+  # YouTube Analytics API
+  # YouTube Data API v3
+  config.api_key = ENV['YOUTUBE_APP_ID']
+end
