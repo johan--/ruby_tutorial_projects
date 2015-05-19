55c85fa13208f94a97be9f71fe685c9e85145f3d
Raymond Sapida <ray.sapida@gmail.com>
2015-05-19 04:31:34 -0700

    Add route and functionality for logging out

--- SocialFreak/spec/features/logging_in_spec.rb
+++ SocialFreak/spec/features/logging_in_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 describe 'Loggin in from twiiter', type: :feature do
   it 'valid hash works' do
     visit '/'
-    expect(page).to have_content('Twitter')
+    expect(page).to have_content 'Twitter'
     mock_auth_twitter
     click_link 'Twitter'
-    expect(page).to have_content('Welcome, mockuser!')
+    expect(page).to have_content 'Welcome, mockuser!'
+    expect(page).to have_content 'Log Out'
+    click_link 'Log Out'
   end
 end
