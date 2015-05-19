7c8b417a40d176561c73008884c725d16bc5c640
Raymond Sapida <ray.sapida@gmail.com>
2015-05-19 04:46:58 -0700

    Add authentication for facebook

--- SocialFreak/spec/features/logging_in_spec.rb
+++ SocialFreak/spec/features/logging_in_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'Loggin in from twiiter', type: :feature do
-  it 'valid hash works' do
+describe 'Loggin in from Omniauth', type: :feature do
+  it 'valid hash works for twitter' do
     visit '/'
     expect(page).to have_content 'Twitter'
     mock_auth_twitter
@@ -9,5 +9,17 @@ describe 'Loggin in from twiiter', type: :feature do
     expect(page).to have_content 'Welcome, mockuser!'
     expect(page).to have_content 'Log Out'
     click_link 'Log Out'
+    expect(page).to have_content 'See you!'
+  end
+
+  it 'valid hash works for facebook' do
+    visit '/'
+    expect(page).to have_content 'Facebook'
+    mock_auth_twitter
+    click_link 'Twitter'
+    expect(page).to have_content 'Welcome, mockuser!'
+    expect(page).to have_content 'Log Out'
+    click_link 'Log Out'
+    expect(page).to have_content 'See you!'
   end
 end
--- SocialFreak/spec/support/omniauth_macros.rb
+++ SocialFreak/spec/support/omniauth_macros.rb
@@ -17,4 +17,23 @@ module OmniauthMacros
       },
     })
   end
+
+  def mock_auth_facebook
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+      'provider' => 'facebook',
+      'uid' => '123545',
+      'info' => {
+        'name' => 'mockuser',
+        'email' => 'mock@example.com',
+        'nickname' => 'mocky',
+        'image' => 'mock_user_thumbnail_url',
+        'phone' => '1234567890',
+      },
+      'credentials' => {
+        'token' => 'mock_token',
+        'refresh_token' => 'mock_refresh',
+        'secret' => 'mock_secret'
+      },
+    })
+  end
 end
