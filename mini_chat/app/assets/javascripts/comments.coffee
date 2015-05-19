5acbf397f72c7788ae67f9c91e4c2dcdc37d4217
Raymond Sapida <ray.sapida@gmail.com>
2015-05-19 00:14:29 -0700

    Add code to secure app for faye functionality

--- mini_chat/app/assets/javascripts/comments.coffee
+++ mini_chat/app/assets/javascripts/comments.coffee
@@ -1,5 +1,12 @@
 window.client = new Faye.Client('/faye')
 
+client.addExtension {
+  outgoing: (message, callback) ->
+  message.ext = message.ext || {}
+  message.ext.csrfToken = $('meta[name=csrf-token]').attr('content')
+  callback(message)
+}
+
 jQuery ->
   client.subscribe '/comments', (payload) ->
     $('#comments').find('.media-list').prepend(payload.message) if payload.message
--- mini_chat/config/application.rb
+++ mini_chat/config/application.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../boot', __FILE__)
+require File.expand_path('../csrf_protection', __FILE__)
 
 require "rails"
 # Pick the frameworks you want:
@@ -17,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module MiniChat
   class Application < Rails::Application
+    config.middleware.use FayeRails::Middleware, extensions: [CsrfProtection.new], mount: '/faye', :timeout => 25
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
