3b2877892d2c8eefe24935df968f9ae150634972
Raymond Sapida <ray.sapida@gmail.com>
2015-05-19 07:41:02 -0700

    Install active_type gem and add more to create action and #upload method

--- YtVideosV3/app/controllers/video_uploads_controller.rb
+++ YtVideosV3/app/controllers/video_uploads_controller.rb
@@ -4,10 +4,17 @@ class VideoUploadsController < ApplicationController
   end
 
   def create
-    if params[:file].present? && params[:title].present? # ... and more checks here
-      # upload video
+    @video_upload = VideoUpload.new(title: params[:video_upload][:title],
+                                    description: params[:video_upload][:description],
+                                    file: params[:video_upload][:file].try(:tempfile).try(:to_path))
+    if @video_upload.save
+      uploaded_video = @video_upload.upload!(current_user)
+
+      # check if the video was uploaded or not
+
+      redirect_to root_url
     else
-      # display an error (and user won't even understand what exactly is wrong)
+      render :new
     end
   end
 end
