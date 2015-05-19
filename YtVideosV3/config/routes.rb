6fc596f825ea7a5ea53ec0144b63432cec6d72c1
Raymond Sapida <ray.sapida@gmail.com>
2015-05-19 07:34:16 -0700

    Create omniauth for google and include sign in and out functionality

--- YtVideosV3/config/routes.rb
+++ YtVideosV3/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :videos, only: [:index, :new, :create]
   root to: 'videos#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
 end
--- YtVideosV3/db/schema.rb
+++ YtVideosV3/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519120916) do
+ActiveRecord::Schema.define(version: 20150519143059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "token"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["uid"], name: "index_users_on_uid", unique: true, using: :btree
 
   create_table "videos", force: :cascade do |t|
     t.string   "link"
