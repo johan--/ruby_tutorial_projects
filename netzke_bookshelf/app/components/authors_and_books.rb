05beb5e3e11a4e8ad60f9c6c3391f66e7dae8b65
Raymond Sapida <ray.sapida@gmail.com>
2015-06-04 00:22:06 -0700

    WIP: Edit components to add js but info is not loading but there is nor error message

--- netzke_bookshelf/app/components/authors_and_books.rb
+++ netzke_bookshelf/app/components/authors_and_books.rb
@@ -1,4 +1,9 @@
 class AuthorsAndBooks < Netzke::Basepack::Viewport
+  js_configure do |c|
+    # For the client-side of our component to include the Javascript code  (see below)
+    c.mixin
+  end
+
   def configure(c)
     super
     c.items = [:authors, :books]
@@ -12,5 +17,17 @@ class AuthorsAndBooks < Netzke::Basepack::Viewport
   component :books do |c|
     c.columns = [:title, :exemplars, :completed]
     c.flex = 1
+
+    # Filter books data by author (component_session is simply the session store
+    # scope out to the AuthorsAndBooks component, and is set in the endpoint
+    # call below)
+    c.scope = {author_id: component_session[:current_author_id]}
+  end
+
+  # This "gets called" by the client side
+  endpoint :server_set_author do |params, this|
+    # params[:author_id] comes from the client side of the component (see the
+    # Javascript code below)
+    component_session[:current_author_id] = params[:author_id]
   end
 end
