c2b7f30520e9694b44023624377d01bcb0b9bdba
Raymond Sapida <ray.sapida@gmail.com>
2015-06-03 20:01:48 -0700

    Add tests for sample email
    http://blog.lucascaton.com.br/index.php/2010/10/25/how-to-test-mailers-in-rails-3-with-rspec/

--- sending_email_rails/spec/mailers/example_mailer_spec.rb
+++ sending_email_rails/spec/mailers/example_mailer_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe ExampleMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'sample email' do
+    let(:user) { User.create(name: 'Lucas', email: 'lucas@email.com') }
+    let(:mail) { ExampleMailer.sample_email(user) }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eql('Sample Email')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eql([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eql(['from@example.com'])
+    end
+
+    it 'assigns @name' do
+      expect(mail.body.encoded).to match(user.name)
+    end
+  end
 end
