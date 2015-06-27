93c45cfc622ff14c1f7c728df7be95749f1e5e1c
Raymond Sapida <ray.sapida@gmail.com>
2015-06-27 16:26:25 -0700

    Add show action and view to matches

--- Doter/app/controllers/matches_controller.rb
+++ Doter/app/controllers/matches_controller.rb
@@ -2,4 +2,8 @@ class MatchesController < ApplicationController
   def index
     @matches = current_user.matches.order('started_at DESC') if current_user
   end
+
+  def show
+    @match = Match.find_by(id: params[:id])
+  end
 end
