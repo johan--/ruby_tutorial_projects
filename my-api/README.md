b4f9b157362cc87ad5263f112317af70531099f1
Raymond Sapida <ray.sapida@gmail.com>
2015-07-22 13:34:40 -0700

    Initial commit of rails-api tutorial

---
+++ my-api/README.md
@@ -0,0 +1,1 @@
+# [Tutorial on writing Rails APIs](http://developingjourney.netlify.com/2015/07/14/start-writing-apis-with-rails)
---
+++ my-api/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+MyApi::Application.config.secret_token = '2c6152358ae5bfd7aaea812ec6198e97fa137df163843c9ddd6bd12a4f4d4d7a46e1128098f5f37ee8a830c792fa5d9ba9d789910e9ed7b47d583e126bef08d6'
