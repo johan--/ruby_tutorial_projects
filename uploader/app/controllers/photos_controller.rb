3613dada9dd331dafd8cd172a12831ab844e78c3
Raymond Sapida <ray.sapida@gmail.com>
2015-07-24 12:32:20 -0700

    Add index new and create actions to photos controller

--- uploader/app/controllers/photos_controller.rb
+++ uploader/app/controllers/photos_controller.rb
@@ -1,2 +1,36 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def new
+    @photo = Photo.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    respond_to do |format|
+      if @photo.save
+        flash[:success] = 'Photo was successfully created.'
+        format.html { redirect_to(@photo) }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :title)
+  end
 end
