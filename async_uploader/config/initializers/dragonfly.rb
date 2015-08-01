93a27de739d10e14f8d6998481c6a0c9637d866d
Raymond Sapida <ray.sapida@gmail.com>
2015-08-01 15:20:39 -0700

    Fix data store value to be inside dragonfly configure block

--- async_uploader/config/initializers/dragonfly.rb
+++ async_uploader/config/initializers/dragonfly.rb
@@ -8,9 +8,17 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  if Rails.env.development? || Rails.env.test?
+    datastore :file,
+      root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+      server_root: Rails.root.join('public')
+  else
+    datastore :s3,
+      bucket_name: 'your_bucket_name',
+      access_key_id: ENV['AWS_KEY'],
+      secret_access_key: ENV['AWS_SEC'],
+      url_scheme: 'https'
+  end
 end
 
 # Logger
@@ -23,16 +31,4 @@ Rails.application.middleware.use Dragonfly::Middleware
 if defined?(ActiveRecord::Base)
   ActiveRecord::Base.extend Dragonfly::Model
   ActiveRecord::Base.extend Dragonfly::Model::Validations
-end
-
-if Rails.env.development? || Rails.env.test?
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
-else
-  datastore :s3,
-    bucket_name: 'your_bucket_name',
-    access_key_id: ENV['AWS_KEY'],
-    secret_access_key: ENV['AWS_SEC'],
-    url_scheme: 'https'
 end
