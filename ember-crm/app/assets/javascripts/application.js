72dc5d28bd84dcdae4d5d72be4bb265bb1102bc4
Raymond Sapida <ray.sapida@gmail.com>
2015-08-04 13:36:19 -0700

    Add ember into rails app
    > rails g ember:bootstrap -n App --javascript-engine js
    > rails g ember:install --tag=v1.5.0 --ember
    > rails g ember:install --tag=v1.0.0-beta.7 --ember-data

--- ember-crm/app/assets/javascripts/application.js
+++ ember-crm/app/assets/javascripts/application.js
@@ -12,4 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery
+//= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+//= require_self
+//= require ./app
+
+// for more details see: http://emberjs.com/guides/application/
+App = Ember.Application.create();
+
 //= require_tree .
