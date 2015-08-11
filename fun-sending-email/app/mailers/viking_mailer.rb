59bbe8bec58cca52404e6e4cc9d4981c243498de
Raymond Sapida <ray.sapida@gmail.com>
2015-08-11 02:29:15 -0700

    Add welcome_email action and view in viking mailer

--- fun-sending-email/app/mailers/viking_mailer.rb
+++ fun-sending-email/app/mailers/viking_mailer.rb
@@ -1,2 +1,8 @@
 class VikingMailer < ApplicationMailer
+  default from: 'bloodyvikings@breakfast.com'
+
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Do you have any spam?')
+  end
 end
