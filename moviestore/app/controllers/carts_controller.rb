5bbbb0a2a06559470f6b09fd0a6ef17bfc40ea9c
Raymond Sapida <ray.sapida@gmail.com>
2015-08-22 15:21:11 -0700

    Add cart page
    - Add carts#show, carts#add, and carts#remove actions.
    - Add carts#show view and link to it from signed in user.
    - Use redis var in carts controller.

---
+++ moviestore/app/controllers/carts_controller.rb
@@ -0,0 +1,32 @@
+class CartsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @movie = Movie.find(params[:id])
+    @cart_action = @movie.cart_action current_user.try :id
+  end
+
+  def add
+    $redis.sadd current_user_cart, params[:movie_id]
+    render json: current_user.cart_count, status: 200
+  end
+
+  def remove
+    $redis.srem current_user_cart, params[:movie_id]
+    render json: current_user.cart_count, status: 200
+  end
+
+  private
+
+  def current_user_cart
+    "cart#{current_user.id}"
+  end
+
+  def cart_action(current_user_id)
+    if $redis.sismember "cart#{current_user_id}", id
+      "Remove from"
+    else
+      "Add to"
+    end
+  end
+end
