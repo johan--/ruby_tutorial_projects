edd1c802ae002c56f672ab1313ea974524badac9
Raymond Sapida <ray.sapida@gmail.com>
2015-08-22 17:07:16 -0700

    Create a transactions controller

---
+++ moviestore/app/controllers/transactions_controller.rb
@@ -0,0 +1,17 @@
+class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_cart!
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+  def check_cart!
+    if current_user.get_cart_movies.blank?
+      redirect_to root_url, alert: "Please add some items to your cart before processing your transaction!"
+    end
+  end
+end
--- moviestore/config/routes.rb
+++ moviestore/config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     put 'add/:movie_id', to: 'carts#add', as: :add_to
     put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
   end
+  resources :transactions, only: [:new, :create]
 end
