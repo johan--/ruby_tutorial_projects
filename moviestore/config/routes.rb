2d610abf4d4f3953a7f98df304f3ee0675a8234c
Raymond Sapida <ray.sapida@gmail.com>
2015-08-22 17:59:04 -0700

    Turn advanced search queries into POST requests

--- moviestore/config/routes.rb
+++ moviestore/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'movies#index'
-  resources :movies, only: [:show, :index]
+  resources :movies, only: [:show, :index] do
+    match :search, to: 'movies#index', via: :post, on: :collection
+  end
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resource :cart, only: [:show] do
     put 'add/:movie_id', to: 'carts#add', as: :add_to
