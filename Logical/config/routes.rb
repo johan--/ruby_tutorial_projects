5f03da79b33f9f156f16b24887bbade28285bde6
Raymond Sapida <ray.sapida@gmail.com>
2015-08-24 03:14:57 -0700

    Create password reset feature
    - Add perishible token to user field for the feature.
    - Create a password reset controller with new, create, edit and update actions.
    - Create a password reset mailer and call it from the controller.
    - Update sign in view with a button for forgotten passwords.

--- Logical/config/routes.rb
+++ Logical/config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 
   delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
   get '/sign_in', to: 'user_sessions#new', as: :sign_in
+
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
--- Logical/db/schema.rb
+++ Logical/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824100409) do
+ActiveRecord::Schema.define(version: 20150824100817) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150824100409) do
     t.datetime "last_login_at"
     t.string   "current_login_ip"
     t.string   "last_login_ip"
+    t.string   "perishable_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
